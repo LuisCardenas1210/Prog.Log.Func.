e9b819bafa11752c291cf939d7ff9b128a372854
LuisCardenas1210 <167260620+LuisCardenas1210@users.noreply.github.com>
2025-02-10 09:52:38 -0600

    Update fizzbuzz.hs

--- fizzbuzz/fizzbuzz.hs
+++ fizzbuzz/fizzbuzz.hs
@@ -1,29 +1,41 @@
 module Main where
-
+{-
+Funcion fizzbuzz:
+verifica si el numero ingresado es multiplo de 3, 5 o ambos con la funcion `mod`
+si no es multiplo, entra a la funcion numeros
+-}
 fizzbuzz :: Int -> String
 fizzbuzz n
     | n `mod` 15 == 0 = "FizzBuzz!"
     | n `mod` 5 == 0 = "Fizz!"
     | n `mod` 3 == 0 = "Buzz!"
-    | otherwise = numberToWords n
-
-numberToWords :: Int -> String
-numberToWords n
-    | n < 20 = units n
-    | n `mod` 10 == 0 = tens (n `div` 10)
-    | otherwise = tens (n `div` 10) ++ " " ++ units (n `mod` 10)
+    | otherwise = numeros
+{-
+Funcion numeros:
+transforma el numero ingresado, en un string del numero en ingles
+si es menor a 20, escribe el numero asignado, si es mayor, concatena 
+las decenas y las unidades
+-}
+numeros :: Int -> String
+numeros n
+    | n < 20 = unidades n
+    | n `mod` 10 == 0 = decenas (n `div` 10)
+    | otherwise = decenas (n `div` 10) ++ " " ++ unidades (n `mod` 10)
   where
-    units x = case x of
+    unidades x = case x of
         0 -> "zero";  1 -> "one";   2 -> "two";   3 -> "three";  4 -> "four"
         5 -> "five";  6 -> "six";   7 -> "seven"; 8 -> "eight";  9 -> "nine"
         10 -> "ten";  11 -> "eleven";  12 -> "twelve";  13 -> "thirteen"; 14 -> "fourteen"
         15 -> "fifteen"; 16 -> "sixteen"; 17 -> "seventeen"; 18 -> "eighteen"; 19 -> "nineteen"
     
-    tens x = case x of
+    decenas x = case x of
         2 -> "twenty"; 3 -> "thirty"; 4 -> "forty"; 5 -> "fifty"
         6 -> "sixty"; 7 -> "seventy"; 8 -> "eighty"; 9 -> "ninety"
         _ -> ""
-
+{-
+la función main muestra el mensaje "ingresa un numero del 1 al 100"
+y no manda a la función fizzbuzz
+-}
 main :: IO ()
 main = do
     putStrLn "Introduce un número entre 0 y 100:"
